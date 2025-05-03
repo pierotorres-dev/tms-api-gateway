994a381deef4ab4c3ab48004ef90c65fb417e5e7
Piero Torres <torresartica1701@gmail.com>
2025-05-03 18:16:53 -0500

    first commit

---
+++ src/main/java/com/dliriotech/tms/apigateway/TmsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dliriotech/tms/apigateway/TmsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dliriotech.tms.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
