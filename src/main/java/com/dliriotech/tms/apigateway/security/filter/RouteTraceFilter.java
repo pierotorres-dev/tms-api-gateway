0cf7977ab013ed0e65752b449a0ec05b797670d7
Piero Torres <torresartica1701@gmail.com>
2025-06-09 10:58:17 -0500

    ci: Add GitHub Actions workflow for deployment

--- src/main/java/com/dliriotech/tms/apigateway/security/filter/RouteTraceFilter.java
+++ src/main/java/com/dliriotech/tms/apigateway/security/filter/RouteTraceFilter.java
@@ -1,4 +1,4 @@
-package com.dliriotech.tms.apigateway.config.filter;
+package com.dliriotech.tms.apigateway.security.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
