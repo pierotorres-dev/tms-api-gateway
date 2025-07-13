7c67e45f5986a607c3f1267ea4098cd1d4faefe6
pierotorres-dev <torresartica1701@gmail.com>
2025-07-13 18:19:36 -0500

    feat: add retry method to routing

--- src/main/java/com/dliriotech/tms/apigateway/config/GatewayRoutingConfig.java
+++ src/main/java/com/dliriotech/tms/apigateway/config/GatewayRoutingConfig.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.Duration;
+
 @Configuration
 public class GatewayRoutingConfig {
 
@@ -19,16 +21,34 @@ public class GatewayRoutingConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("auth-service", r -> r.path("/api/auth/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(authServiceUrl))
                 .route("auth-service", r -> r.path("/api/tokens/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(authServiceUrl))
                 .route("auth-service", r -> r.path("/api/users/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(authServiceUrl))
-                .route("fleet-service", r -> r.path("/api/v1/equipos/**")
+                .route("fleet-service-equipos", r -> r.path("/api/v1/equipos/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(fleetServiceUrl))
                 .route("fleet-service", r -> r.path("/api/v1/observaciones-equipo/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(fleetServiceUrl))
                 .route("fleet-service", r -> r.path("/api/v1/catalogos/**")
+                        .filters(f -> f.retry(config -> config
+                                .setRetries(3)
+                                .setBackoff(Duration.ofMillis(100), Duration.ofSeconds(1), 2, true)))
                         .uri(fleetServiceUrl))
                 .build();
     }
